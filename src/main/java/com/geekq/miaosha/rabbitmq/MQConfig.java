123d4cd3f1334260104706797c9ada474c8a5c6b
SavorTheFlavor <2500364530@qq.com>
2019-02-21 00:12:53 +0800

    rabbitmq的几种工作模式

--- src/main/java/com/geekq/miaosha/rabbitmq/MQConfig.java
+++ src/main/java/com/geekq/miaosha/rabbitmq/MQConfig.java
@@ -24,11 +24,26 @@ public class MQConfig {
 	public static final String QUEUE = "queue";
 	public static final String TOPIC_QUEUE1 = "topic.queue1";
 	public static final String TOPIC_QUEUE2 = "topic.queue2";
+	public static final String TOPIC_QUEUE3 = "topic.#"; // '#'匹配0个或多个单词
 	public static final String HEADER_QUEUE = "header.queue";
 	public static final String TOPIC_EXCHANGE = "topicExchage";
 	public static final String FANOUT_EXCHANGE = "fanoutxchage";
 	public static final String HEADERS_EXCHANGE = "headersExchage";
-	
+
+	/*
+	rabbitmq的五种工作模式模式
+
+	简单模式：一个生产者，一个消费者
+
+	work模式：一个生产者，多个消费者，每个消费者获取到的消息唯一。
+
+	订阅模式：一个生产者发送的消息会被多个消费者获取。
+
+	路由模式：发送消息到交换机并且要指定路由key ，消费者将队列绑定到交换机时需要指定路由key
+
+	topic模式：将路由键和某模式进行匹配，此时队列需要绑定在一个模式上，“#”匹配一个词或多个词，“*”只匹配一个词。
+
+	 */
 	/**
 	 * Direct模式 交换机Exchange
 	 * */
@@ -91,6 +106,7 @@ public class MQConfig {
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("header1", "value1");
 		map.put("header2", "value2");
+		//满足这些条件的消息才会被发送
 		return BindingBuilder.bind(headerQueue1()).to(headersExchage()).whereAll(map).match();
 	}
 	
